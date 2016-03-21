cdf87facb3655044ab1a389e354e951b5e29cdd5
Tifara Ramelson <tifararamelson@gmail.com>
2016-03-21 01:38:49 -0400

    adds 'None' to home page when user isn't enrolled in any courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,4 @@
 class Course < ActiveRecord::Base
 	has_many :enrollments
-	has_many :users, through: :enrollments
+	has_many :users, -> { distinct }, through: :enrollments
 end
--- app/models/user.rb~
+++ app/models/user.rb~
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   include Clearance::User
+	has_many :enrollments
+	has_many :courses, through: :enrollments
 end
