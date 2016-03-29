3b8d15b20ca7966eb0bdb4fc9fb5ca104645a2ca
Tifara Ramelson <tifararamelson@gmail.com>
2016-03-28 22:22:03 -0400

    no longer displays duplicate courses on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,8 +5,12 @@ class HomeController < ApplicationController
 	@enrollments = Enrollment.all
 
 	@course_enrolled = params[:course]
+	
+	puts "**********************"
+	puts Enrollment.where(course_id: params[:id]).empty?
+	puts "***********************"
 
-	if @course_enrolled
+	if @course_enrolled && Enrollment.where(course_id: params[:id]).empty?
 		#need to add new course to enrollments
 		Enrollment.create(course_id: params[:id], user_id: current_user.id)
 		@enrollments = Enrollment.all
