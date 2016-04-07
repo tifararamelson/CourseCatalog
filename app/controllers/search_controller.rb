3e5dde113e4749adc97b731f079255e54a6e0874
Tifara Ramelson <tifararamelson@gmail.com>
2016-04-06 20:28:56 -0400

    no error when hitting enter

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,6 @@
 class SearchController < ApplicationController
 
   before_action :require_login
-  #skip_before_action :verify_authenticity_token
 
   def index
   	@courses = Course.all
@@ -9,32 +8,17 @@ class SearchController < ApplicationController
   end
 
   def results
-    #respond_to :js
-
   	@courses = Course.all
   	@course_result = params[:s]
-
 
   	@subjects = Subject.all
   	@sub_id = @subjects.find(params[:subject_id]).s_id
   	@sub_result = @subjects.find(params[:subject_id]).name
 
-    #@javascript
-
-    #render "results.js.erb"
-   #render :js => @courses
 
     respond_to do |format|
-      #format.html { render nothing: true, status: :unauthorized }
-      format.js #{ render 'results' }
-     # format.html { render(:text => "not implemented") }
-      
+      format.js
     end
-      #format.html { render 'new'}
-     # format.js { render 'results.js.erb'}
-      #$.ajax url: "/uploads/refresh_table", format: 'js'
-
-   # end
   end
 
 end
